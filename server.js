79037b0f572080e80da16b3a0fb9b38acacd82e8
Artur Adib <arturadib@gmail.com>
2011-07-22 09:12:47 -0400

    First working MongoDB

--- server.js
+++ server.js
@@ -1,63 +1,59 @@
-//
-// server.js for The Wall
-//
+/*
+ *
+ *
+ * Server for The Wall
+ * By Artur B Adib, 2011
+ *
+ */
 
-var express = require('express');
+var express = require('express')
+  , Mongolian = require('mongolian')
+  , ObjectId = require('mongolian').ObjectId;
+
+// Environment
+var MAXSIZE = 3; // # of posts per request
 var port = process.env.PORT || 8989;
 
+// MongoDB setup
+var db = (new Mongolian()).db('thewall');
+var posts = db.collection('posts');
+
+// Express setup
 var app = express.createServer();
 app.use(express.bodyParser());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 //
-// Data store
-//
-var defaultPost = {
-  msg: "It seems like our servers were restarted, so previous posts were lost. How about adding your own? :)",
-  avatarPath: "img/halloween_slimer_icon.png",
-  name:"TheWall-Bot",
-  time:(new Date()).getTime(),
-  id:1
-};
-var posts = [defaultPost];
-var id = 2;
-var MAXSIZE = 25;
-
-//
 // POST /api/posts
 //
 app.post('/api/posts', function(req, res){
   var entry = req.body;
-  entry.id = id++;
 
-  posts.push(entry);
-  if (posts.length > MAXSIZE) {
-    posts.shift();
-  }
-
-  // response
-  res.send({id:entry.id}, 201); // 201 == Created
+  // db: save
+  posts.save(entry, function(err, finalEntry){
+    var _id = finalEntry._id.toString();
+    res.send({id:_id}, 201); // 201 == Created
+  });
 });
 
 //
 // GET /api/posts
 //
 app.get('/api/posts', function(req, res){
-  if (req.query.since) {
-    var _posts = [];
-    posts.forEach(function(post){
-      if (post.time > req.query.since) {
-        _posts.push(post);
-      }
+  var _posts; // result Array
+
+  if (req.query.since && req.query.since > 1) {
+    posts.find({time:{$gt:req.query.since}}).sort({time:-1}).limit(MAXSIZE).toArray(function(err, _posts){
+      res.send(_posts.reverse()); // so latest posts come last
     });
-    res.send(_posts);
   }
   else {
-    res.send(posts);
+    posts.find().sort({time:-1}).limit(MAXSIZE).toArray(function(err, _posts){
+      res.send(_posts.reverse()); // so latest posts come last
+    });
   }
 });
-
 
 app.listen(port);
 
